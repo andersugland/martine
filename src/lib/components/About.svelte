031501529f81efaa0d3eac7ac6dd84a0d9e28328
Anders Ugland <ugland.anders@gmail.com>
2023-09-12 20:50:35 +0200

    Minor responsive styling tweak

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -2,7 +2,7 @@
 	export let title, excerpt;
 </script>
 
-<section class="mt-0 pt-md pb-[150px] bg-dark text-offwhite selection:bg-offwhite selection:text-dark">
+<section class="mt-0 pt-md pb-md md:pb-[150px] bg-dark text-offwhite selection:bg-offwhite selection:text-dark">
 	<div class="container px-4 grid md:grid-cols-12 items-center">
 		<div class="col-span-full md:col-start-4 md:col-span-9 lg:col-start-6 lg:col-span-7">
 			<h2 class="text-2xl-clamp leading-[1.15em]">{title}</h2>

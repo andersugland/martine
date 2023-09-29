dfb84f538c1454ef921c9290f1af21b516b3dd52
Anders Ugland <ugland.anders@gmail.com>
2023-09-29 21:46:41 +0200

    Move fonts to package manager

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,7 @@
 <script>
+	import '@fontsource/poppins/100.css';
+	import '@fontsource/poppins/200.css';
+	import '@fontsource/prata/400.css';
 	import '../app.css';
 	import Header from './Header.svelte';
 	import Footer from './Footer.svelte';

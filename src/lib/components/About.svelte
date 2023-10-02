e6769bf47e9e0fa894a8f1b9c6ba62f7e3b13072
Anders Ugland <ugland.anders@gmail.com>
2023-10-02 23:21:44 +0200

    Minor styling

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -6,12 +6,12 @@
 </script>
 
 <section class="flex flex-col mt-0 bg-dark text-offwhite selection:bg-offwhite selection:text-dark">
-	<Image maxWidth={2560} maxHeight={1440} image={image} quality={100} styling="w-full h-full max-h-[750px] object-cover" />
+	<Image maxWidth={2560} maxHeight={1440} image={image} quality={100} styling="w-full h-full aspect-square md:aspect-auto max-h-[550px] md:max-h-[750px] object-cover" />
 	
 	<div class="container px-4 pt-md pb-md md:pb-[150px] grid md:grid-cols-12 items-center">
-		<div class="col-span-full md:col-start-4 md:col-span-9 lg:col-start-6 lg:col-span-7">
+		<div class="col-span-full md:col-start-4 md:col-span-9 lg:col-start-6 lg:col-span-7 ">
 			<h2 class="text-2xl-clamp leading-[1.15em]">{title}</h2>
-			<p class="text-md font-thin mt-sm">{excerpt}</p>
+			<p class="text-base md:text-md font-thin mt-sm">{excerpt}</p>
 		</div>
 	</div>
 </section>
--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -5,7 +5,7 @@
 <section class="container px-4 min-h-screen grid items-center">
 	<div class="grid md:grid-cols-12 items-center">
 		<h1 class="text-3xl-clamp leading-[1.15em] col-span-full">{title}</h1>
-		<div class="md:col-start-2 md:col-span-10 text-md font-thin mt-sm md:mt-md ml-4 md:ml-0">
+		<div class="md:col-start-2 md:col-span-10 text-[1.25rem] md:text-md font-thin mt-sm md:mt-md ml-4 md:ml-0">
 			<p class="border-l-2 border-dark pl-4 md:pl-10">{excerpt}</p>
 		</div>
 	</div>
